7dd08c602adf698792bfeb8191c8df669dcb9a2d
Chinara Orujova <china.orujova@gmail.com>
2022-01-10 12:30:31 -0500

    work 2 is done

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // doing some work here (you can type anything you want)
 
+        //doing awesome work 2!
+
     }
 }
